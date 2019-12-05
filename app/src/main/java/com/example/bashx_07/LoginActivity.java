1d2601e414d0c37d16be4fe57c8c77962146e111
unknown <Admin@IHR80PBF431.continental.edu.pe>
2019-12-04 21:42:31 -0500

    Con Scroll - Login

---
+++ app/src/main/java/com/example/bashx_07/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.bashx_07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.bashx_07.models.Users;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText txtUser;
+    EditText txtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtUser = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (Users.passwordInValid(txtUser.getText().toString(),txtPassword.getText().toString())) {
+                    Intent o = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(o);
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/bashx_07/SplashScreenActivity.java
+++ app/src/main/java/com/example/bashx_07/SplashScreenActivity.java
@@ -11,7 +11,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Intent o = new Intent(this,AuthActivity.class);
+        Intent o = new Intent(this,LoginActivity.class);
         startActivity(o);
         finish();
     }
